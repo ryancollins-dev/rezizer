2d7c60ad83de8cf422c669805beffff9fdef3df2
ryancollins-dev <email@ryd3v.rocks>
2022-10-12 19:22:58 -0400

    update

--- main.js
+++ main.js
@@ -4,13 +4,13 @@
 // use npx electronmon .
 
 const path = require('path');
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 const isMac = process.platform === 'darwin';
 const isDev = process.env.NODE_ENV !== 'production';
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
     title: 'Image Rezizer',
-    width: isDev ? 900 : 500,
+    width: isDev ? 950 : 500,
     height: 600,
   });
 
@@ -22,8 +22,24 @@ function createMainWindow() {
   mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
 }
 
+// Create about window
+function createAboutWindow() {
+  const aboutWindow = new BrowserWindow({
+    title: 'About Image Rezizer',
+    width: 300,
+    height: 300,
+  });
+
+  aboutWindow.loadFile(path.join(__dirname, './renderer/about.html'));
+}
+
+// App is ready
 app.whenReady().then(() => {
   createMainWindow();
+
+  // Implement menu
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
@@ -31,6 +47,39 @@ app.whenReady().then(() => {
     }
   });
 });
+
+// Menu template
+const menu = [
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            {
+              label: 'About',
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+  {
+    role: 'fileMenu',
+  },
+  ...(!isMac
+    ? [
+        {
+          label: 'Help',
+          submenu: [
+            {
+              label: 'About',
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+];
 
 app.on('window-all-closed', () => {
   if (!isMac) {
