aa2b1686ddc539efa396533981e8a3bc719ce80e
ryancollins-dev <email@ryd3v.rocks>
2022-10-12 00:13:40 -0400

    update

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
+// Author: @ryd3v
+// Date: Oct 11, 2022
+
+// use npx electronmon .
+
 const path = require('path');
 const { app, BrowserWindow } = require('electron');
 const isMac = process.platform === 'darwin';
-
+const isDev = process.env.NODE_ENV !== 'production';
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
     title: 'Image Rezizer',
-    width: 500,
+    width: isDev ? 900 : 500,
     height: 600,
   });
+
+  // Open devtools if in dev env
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+
   mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
 }
 
